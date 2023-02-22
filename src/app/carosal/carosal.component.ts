41da673dc332c03edb755a41297f152af00578fb
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-02-22 11:23:23 +0000

    update product.component

--- src/app/carosal/carosal.component.ts
+++ src/app/carosal/carosal.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProductService } from '../../services/product.service';
+
 
 @Component({
   selector: 'app-carosal',
@@ -8,14 +8,5 @@ import { ProductService } from '../../services/product.service';
 })
 export class CarosalComponent {
 
- products!: any[];
-
-  constructor(private productService: ProductService) { }
-
-  ngOnInit() {
-    this.productService.getProducts().subscribe(products => {
-      this.products = products;
-    });
-  }
 
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+  products!: any[];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+    });
+  }
 
 }
