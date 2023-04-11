83b8a5453f58f7f2879f0158676bb859f756df13
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-04-11 12:13:16 +0000

    Update

--- src/app/price/price.component.ts
+++ src/app/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PriceService } from '../../services/price.service';
 
 @Component({
   selector: 'app-price',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./price.component.scss']
 })
 export class PriceComponent {
+  price: any;
+  prices: any;
 
+  constructor(private priceService: PriceService) { }
+
+  ngOnInit() {
+    this.priceService.getPrices().subscribe((price: any) => {
+      this.prices = price;
+      console.log(this.prices);
+    });
+  }
 }
