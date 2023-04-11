54f18abe16906f2895aa2ae2485453f2ed88f5e0
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-04-11 12:56:31 +0000

    Update

--- src/app/price/price.component.ts
+++ src/app/price/price.component.ts
@@ -7,15 +7,21 @@ import { PriceService } from '../../services/price.service';
   styleUrls: ['./price.component.scss']
 })
 export class PriceComponent {
-  price: any;
   prices: any;
+  displayedPrices: any;
 
   constructor(private priceService: PriceService) { }
 
   ngOnInit() {
     this.priceService.getPrices().subscribe((price: any) => {
       this.prices = price;
+      this.displayedPrices = this.prices.slice(0, 10); // Assign initial 10 rows to 'displayedPrices' array
       console.log(this.prices);
     });
+  }
+
+  loadMore() {
+    // Load more data from 'prices' array and append to 'displayedPrices' array
+    this.displayedPrices = this.displayedPrices.concat(this.prices.slice(this.displayedPrices.length, this.displayedPrices.length + 20));
   }
 }
