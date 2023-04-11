9127c8d2ac5d05cb9fb8176da9239ebb99c321e8
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-04-11 09:12:24 +0000

    Create price component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './about/about.component';
 import { CarosalComponent } from './carosal/carosal.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { PriceComponent } from './price/price.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     ContainerComponent,
     ProductComponent,
     AboutComponent,
-    CarosalComponent
+    CarosalComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
