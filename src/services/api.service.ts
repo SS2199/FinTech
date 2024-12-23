32ba44a842b5c82eab030f7a4dd252bc89681709
sd ss <95736922+SS2199@users.noreply.github.com>
2024-12-23 16:39:18 +0530

    Update api.service.ts

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-    private apiUrl = 'http://localhost:5000/items';
+    private apiUrl = 'https://celescontainerwebapp-server.mongo.cosmos.azure.com:443/items';
 
 
   constructor(private http: HttpClient) {}
